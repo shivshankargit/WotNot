5ccce57a7d3043d4b4562e655c19df79eacb86ae
Bharat00007 <taklikarbharat8@gmail.com>
2024-08-28 20:27:22 +0530

    created manage_template

--- backend/wati/database/database.py
+++ backend/wati/database/database.py
@@ -6,7 +6,9 @@ from fastapi import Depends
 
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Denmarks123$@localhost/wati_clone'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Denmarks123$@localhost/wati_clone'
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://Bharat:Draken12@localhost:5432/watiDB'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- backend/wati/services/tasks.py
+++ backend/wati/services/tasks.py
@@ -9,7 +9,7 @@ import json
 from dramatiq.middleware import Middleware,SkipMessage
 
 # SQLAlchemy Database Configuration
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Denmarks123$@localhost/wati_clone'
+SQLALCHEMY_DATABASE_URL = 'postgresql://Bharat:Draken12@localhost:5432/watiDB'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
